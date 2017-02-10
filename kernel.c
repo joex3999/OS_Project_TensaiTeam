434addf5e601bf3c0968cb06d937b29d041d358c
youssef <joex3999@gmail.com>
2017-02-10 18:46:13 +0200

    MileStone0

---
+++ kernel.c
@@ -0,0 +1,18 @@
+int main(){
+	interrupt(0x10,0xE*256+'H',0,0,0);
+	interrupt(0x10,0xE*256+'e',0,0,0);
+	interrupt(0x10,0xE*256+'l',0,0,0);
+	interrupt(0x10,0xE*256+'l',0,0,0);
+	interrupt(0x10,0xE*256+'o',0,0,0);
+	interrupt(0x10,0xE*256+' ',0,0,0);
+	interrupt(0x10,0xE*256+'W',0,0,0);
+	interrupt(0x10,0xE*256+'o',0,0,0);
+	interrupt(0x10,0xE*256+'r',0,0,0);
+	interrupt(0x10,0xE*256+'l',0,0,0);
+	interrupt(0x10,0xE*256+'d',0,0,0);
+	interrupt(0x10,0xE*256+'!',0,0,0);
+	while(1){
+
+	}
+	return 0 ;
+}
