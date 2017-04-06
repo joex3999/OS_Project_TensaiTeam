6b17b6b27173868012973c107ab92441ffc76483
youssef <joex3999@gmail.com>
2017-04-06 02:51:58 +0200

    Copy File && Debugging

--- shell.c
+++ shell.c
@@ -8,8 +8,9 @@ int DIV(int,int );
 int getRemainder(int, int);
 void stringSplit(char*,char*,char*);
 void printNum(int);
+int getSectorNum(char*);
 int main(){
-        char line[30];
+        char line[40];
         char buffer[13312];
         char sector[512];
         int first = 0;
@@ -18,7 +19,7 @@ int main(){
         int fourth = 0;
         int fifth = 0;
         int sixth =0;
-        char word [30];
+        char word [40];
         while(1) {
                 interrupt(0x21, 0, "SHELL:>\0", 0, 0); /*print out the file*/
                 interrupt(0x21,1,line,0,0);
@@ -105,7 +106,7 @@ int string_compare(char* str1, char* str2)
         }
 }
 void getFiles(char* pointer){
-        int i;  
+        int i;
         int s;
         int sum=0;
         for(i = 0; i<512; i+=32) {
@@ -113,10 +114,20 @@ void getFiles(char* pointer){
                         interrupt(0x10,0xE*256+'\b',0,0,0);
                         interrupt(0x10,0xE*256+'\b',0,0,0);
                         interrupt(0x10,0xE*256+'\b',0,0,0);
+                        interrupt(0x10,0xE*256+'\b',0,0,0);
+                        interrupt(0x10,0xE*256+'\b',0,0,0);
+                        interrupt(0x10,0xE*256+'\b',0,0,0);
+                        interrupt(0x10,0xE*256+'\b',0,0,0);
+                        interrupt(0x10,0xE*256+'\b',0,0,0);
                         interrupt(0x10,0xE*256+'$',0,0,0);
                         interrupt(0x10,0xE*256+'\0',0,0,0);
                         for(s=0; s<6; s++) {
+                          if(*(pointer+i+s)>=48&&*(pointer+i+s)<=122)
                                 interrupt(0x10,0xE*256+*(pointer+i+s),0,0,0);
+                                else{
+                                    interrupt(0x10,0xE*256+' ',0,0,0);
+                                }
+
 
                         }
                         s=6;
@@ -131,6 +142,8 @@ void getFiles(char* pointer){
                         interrupt(0x10,0xE*256+' ',0,0,0);
                         interrupt(0x10,0xE*256+' ',0,0,0);
                         interrupt(0x10,0xE*256+' ',0,0,0);
+                        interrupt(0x10,0xE*256+' ',0,0,0);
+                        interrupt(0x10,0xE*256+' ',0,0,0);
                         printNum(sum);
                         interrupt(0x10,0xE*256+'\n',0,0,0);
 
@@ -138,7 +151,7 @@ void getFiles(char* pointer){
                         interrupt(0x10,0xE*256+'\0',0,0,0);
                 }
 
-              //  i+=31;
+
         }
 }
 void copy(char*str1, char* str2,int s){
@@ -153,17 +166,21 @@ void copy(char*str1, char* str2,int s){
 }
 
 void stringSplit(char*str1, char* str2, char* str3 ){
-        str1++;
+
         while(*str1!=' ') {
                 *str2=*str1;
                 str2++;
                 str1++;
+
         }
 
+
+    *str1++;
         while(*str1!='\0') {
                 *str3=*str1;
                 str3++;
                 str1++;
+
         }
 
         *str2='\0';
@@ -215,40 +232,133 @@ void copyCommand(char* str1){
         int x=0;
         int i=0;
         char b1[13312];
-        char name1[6];
-        char name2[6];
+        char name1[20];
+        char name2[20];
+        char sector[512];
         char toString [7];
         int secNum =0;
+        char c[55];
 
-        interrupt(0x21, 0, str1, 0, 0);
 
-
+        interrupt(0x21, 2,sector, 2, 0);
         stringSplit(str1,name1,name2);
 
-        interrupt(0x21, 0, name1, 0, 0);
-        interrupt(0x21, 0, "\n\0", 0, 0);
-        interrupt(0x21, 0, name2, 0, 0);
+        secNum = getSectorNum(name1,sector);
+        if(secNum!=-1){
+      interrupt(0x21, 3,name1, b1, 0);
+      interrupt(0x21, 8, name2, b1, secNum);
 
-        b1[0]=0x00;
-        x=1;
-        interrupt(0x21, 3,name1, b1, 0);
+                              c[0]='F';
+                              c[1]='i';
+                              c[2]='l';
+                              c[3]='e';
+                              c[4]=' ';
+                              c[5]='c';
+                              c[6]='o';
+                              c[7]='p';
+                              c[8]='i';
+                              c[9]='e';
+                              c[10]='d';
+                              c[11]=' ';
+                              c[12]='s';
+                              c[13]='u';
+                              c[14]='c';
+                              c[15]='c';
+                              c[16]='e';
+                              c[17]='s';
+                              c[18]='s';
+                              c[19]='f';
+                              c[20]='u';
+                              c[21]='l';
+                              c[22]='l';
+                              c[23]='y';
+                              c[24]='\n';
+                              c[25]='\b';
+                              c[26]='\b';
+                              c[27]='\b';
+                              c[28]='\b';
+                              c[29]='\b';
+                              c[30]='\b';
+                              c[31]='\b';
+                              c[32]='\b';
+                              c[33]='\b';
+                              c[34]='\b';
+                              c[35]='\b';
+                              c[36]='\b';
+                              c[37]='\b';
+                              c[38]='\b';
+                              c[39]='\b';
+                              c[40]='\b';
+                              c[41]='\b';
+                              c[42]='\b';
+                              c[43]='\b';
+                              c[44]='\b';
+                              c[45]='\b';
+                              c[46]='\b';
+                              c[47]='\b';
+                              c[48]='\b';
+                              c[49]='\b';
+                              c[50]='\b';
+                              c[51]='\b';
+                              c[52]='\b';
+                              c[53]='\0';
 
-        if(b1[0]!=0x00 ) {
-                while(b1[x]!=0x00 && x < 13312) {
-                        secNum++;
-                        x++;
-                }
-                secNum++;
-                secNum=DIV(secNum,512);
-                secNum++;
-                interrupt(0x21, 8, name2, b1, secNum);
-                interrupt(0x21, 0, "Copy Successful\n\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\0", 0, 0);
-                return;
+                              interrupt(0x21, 0, c, 0, 0);
+    }
+      else
+      {
 
+                                c[0]='F';
+                                c[1]='i';
+                                c[2]='l';
+                                c[3]='e';
+                                c[4]=' ';
+                                c[5]='D';
+                                c[6]='o';
+                                c[7]='e';
+                                c[8]='s';
+                                c[9]=' ';
+                                c[10]='n';
+                                c[11]='o';
+                                c[12]='t';
+                                c[13]=' ';
+                                c[14]='e';
+                                c[15]='x';
+                                c[16]='i';
+                                c[17]='s';
+                                c[18]='t';
+                                c[19]='\n';
+                                c[20]='\b';
+                                c[21]='\b';
+                                c[22]='\b';
+                                c[23]='\b';
+                                c[24]='\b';
+                                c[25]='\b';
+                                c[26]='\b';
+                                c[27]='\b';
+                                c[28]='\b';
+                                c[29]='\b';
+                                c[30]='\b';
+                                c[31]='\b';
+                                c[32]='\b';
+                                c[33]='\b';
+                                c[34]='\b';
+                                c[35]='\b';
+                                c[36]='\b';
+                                c[37]='\b';
+                                c[38]='\b';
+                                c[39]='\b';
+                                c[40]='\b';
+                                c[41]='\b';
+                                c[42]='\b';
+                                c[43]='\b';
+                                c[44]='\b';
+                                c[45]='\b';
+                                c[46]='\b';
+                                c[47]='\0';
 
-        }else{
-                interrupt(0x21, 0, "Could not find file to Copy From\n\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\0", 0, 0);
-        }
+                                interrupt(0x21, 0, c, 0, 0);
+      }
 
         return;
 
@@ -307,4 +417,39 @@ void printNum(int num){
           if(arr[pos]!=-1)
             interrupt(0x10,0xE*256+(arr[pos]+48),0,0,0);
         }
+}
+
+int getSectorNum(char* name,char* pointer){
+  int i;
+  int s;
+  int sum=0;
+  int brek = 0 ;
+  for(i = 0; i<512; i+=32) {
+          if(*(pointer+i)!=0&&*(pointer+i)==*name) {
+
+                  for(s=0; s<6&&*(pointer+i+s)!=0; s++) {
+                      if(*(pointer+i+s)!=*(name+s))
+                      {
+                        brek = 1 ;
+                        break;
+                      }
+
+                  }
+                  if(brek)
+                  continue;
+                  s=6;
+                  sum = 0 ;
+                  while(s<32){
+                    if(*(pointer+i+s)!=0){
+                      sum++;
+                    }else
+                    break;
+                    s++;
+                  }
+                  return sum ;
+          }
+
+
+  }
+  return -1 ;
 }
